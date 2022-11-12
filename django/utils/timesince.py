42eaabb6743c2e222f35f120b9ff2ea34a3725c1
Gianpaolo <gianpaolo@protonmail.com>
2022-11-12 13:37:48 +0100

    Implement the optimal solution

--- django/utils/timesince.py
+++ django/utils/timesince.py
@@ -1,4 +1,3 @@
-import calendar
 import datetime
 
 from django.utils.html import avoid_wrapping
@@ -14,46 +13,16 @@ TIME_STRINGS = {
     "minute": ngettext_lazy("%(num)d minute", "%(num)d minutes", "num"),
 }
 
-TIMESINCE_CHUNKS = (
-    (60 * 60 * 24 * 365, "year"),
-    (60 * 60 * 24 * 30, "month"),
-    (60 * 60 * 24 * 7, "week"),
-    (60 * 60 * 24, "day"),
-    (60 * 60, "hour"),
-    (60, "minute"),
-)
+TIME_STRING_KEYS = list(TIME_STRINGS.keys())
+
+TIME_CHUNKS = [
+    60 * 60 * 24 * 7,  # week
+    60 * 60 * 24,  # day
+    60 * 60,  # hour
+    60,  # minute
+]
 
 MONTHS_DAYS = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-
-
-def timesince_months(d, now):
-    months = now.month - d.month
-    if months > 0:
-        if d.day > now.day:
-            months -= 1
-    elif months < 0:
-        months += 12
-        if d.day > now.day:
-            months -= 1
-    elif d.day > now.day:
-        months = 11
-    if months == 0:
-        # doesn't matter the month avg time since there are 0
-        return (0, 30 * 24 * 60 * 60)
-    month = (d.month + months) % 12
-    if month == 0:
-        month = 12
-    oneyear = datetime.timedelta(days=365)
-    pivot = datetime.datetime(
-        now.year, month, min(MONTHS_DAYS[month - 1], d.day), tzinfo=now.tzinfo
-    )
-    if pivot > now:
-        pivot -= oneyear
-    d_adj = datetime.datetime(now.year, d.month, d.day, tzinfo=now.tzinfo)
-    if d_adj > now:
-        d_adj -= oneyear
-    total = (pivot - d_adj).total_seconds()
-    return (months, (total / months))
 
 
 def timesince(d, now=None, reversed=False, time_strings=None, depth=2):
@@ -92,33 +61,44 @@ def timesince(d, now=None, reversed=False, time_strings=None, depth=2):
         d, now = now, d
     delta = now - d
 
-    # Deal with leapyears by subtracing the number of leapdays
-    leapdays = calendar.leapdays(d.year, now.year)
-    if leapdays != 0:
-        if calendar.isleap(d.year):
-            leapdays -= 1
-        elif calendar.isleap(now.year):
-            leapdays += 1
-    delta -= datetime.timedelta(leapdays)
-
-    time_chunks = list(TIMESINCE_CHUNKS)
-    months, avg = timesince_months(d, now)
-    time_chunks[1] = (avg, "month")
-
     # ignore microseconds
     since = delta.days * 24 * 60 * 60 + delta.seconds
-
     if since <= 0:
         # d is in the future compared to now, stop processing.
         return avoid_wrapping(time_strings["minute"] % {"num": 0})
 
-    for i, (seconds, name) in enumerate(time_chunks):
-        if name == "month":
-            # count might overshoot if now.month is longer than the calculated length
-            count = months
-        else:
-            count = since // seconds
-        if count != 0:
+    # get years and months
+    total_months = (now.year - d.year) * 12 + (now.month - d.month)
+    if d.day > now.day:
+        total_months -= 1
+    years, months = divmod(total_months, 12)
+    # calculate the remaining time
+    if years or months:
+        pivot_month = d.month + months
+        pivot_year = d.year + years
+        if pivot_month > 12:
+            pivot_month -= 12
+            pivot_year += 1
+        pivot = datetime.datetime(
+            pivot_year,
+            pivot_month,
+            min(MONTHS_DAYS[pivot_month - 1], d.day),
+            d.hour,
+            d.minute,
+        )
+    else:
+        pivot = d
+    remaining_time = (now - pivot).total_seconds()
+    partials = [years, months]
+
+    for chunk in TIME_CHUNKS:
+        count = remaining_time // chunk
+        partials.append(count)
+        remaining_time -= chunk * count
+
+    i = 0
+    for i, value in enumerate(partials):
+        if value != 0:
             break
     else:
         return avoid_wrapping(time_strings["minute"] % {"num": 0})
@@ -126,18 +106,15 @@ def timesince(d, now=None, reversed=False, time_strings=None, depth=2):
     result = []
     current_depth = 0
 
-    while i < len(time_chunks) and current_depth < depth:
-        seconds, name = time_chunks[i]
-        count = since // seconds
-        if name == "month":
-            # count might overshoot if now.month is longer than the calculated length
-            count = min(count, months)
-        if count == 0:
+    while i < len(TIME_STRING_KEYS) and current_depth < depth:
+        value = partials[i]
+        if value == 0:
             break
-        result.append(avoid_wrapping(time_strings[name] % {"num": count}))
-        since -= seconds * count
+        name = TIME_STRING_KEYS[i]
+        result.append(avoid_wrapping(time_strings[name] % {"num": value}))
         current_depth += 1
         i += 1
+
     return gettext(", ").join(result)
 
 
